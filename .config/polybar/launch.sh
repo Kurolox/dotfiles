504583584779740902318b345cf6be7fc2cc5f3a
Carlos "Kurolox" Gomez <contact@carlosg.dev>
2020-07-10 14:02:56 +0200

    Added support for multiple monitors
    The configuration is somewhat hardcoded to my own setup, since it
    assumes that your main monitor is labeled 'eDP1' and that any other
    secondary monitors are labeled 'HDMI2' or 'DP1'.
    
    This works properly with autorandr, in any setup from one to three
    screens. Right now the polybar in all the bars are the same, though it's
    easy to modify to make different polybar bars show up in different
    monitors.

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -7,6 +7,9 @@ killall -q polybar
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # Launch Polybar, using default config location ~/.config/polybar/config
-polybar example &
+for screen in $(bspc query -M --names)
+do
+    polybar $screen &
+done
 
 echo "Polybar launched..."
